214400cb4a8b440ccb50bd68c7ed27cdac8a71bc
FRPMan <55295923+FRPMan@users.noreply.github.com>
2019-09-13 23:13:21 +0200

    Add Mod File 1

---
+++ hoi4rebalanced.mod
@@ -0,0 +1,9 @@
+name="HOI4 Rebalanced"
+path="mod/hoi4rebalanced/"
+tags={
+	"Gameplay"
+	"Historical"
+	"Fixes"
+}
+remote_file_id="1773237534"
+supported_version="1.7.1"
---
+++ hoi4rebalanced/common/defines/HOI_defines.lua
@@ -0,0 +1,8 @@
+NDefines.NCountry.MIN_STABILITY = -1.0
+NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 8;				-- how many armies a field marshall is limited to. 0 = inf, < 0 = blocked
+NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 48;			-- how many divisions a field marshall is limited to. 0 = inf, < 0 = blocked
+NDefines.NMilitary.CORPS_COMMANDER_ARMIES_CAP = -1;			-- how many armies a corps commander is limited to. 0 = inf, < 0 = blocked
+NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 24;			-- how many divisions a corps commander is limited to. 0 = inf, < 0 = blocked
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 999;			--Max army experience a country can store		#HOI4TGW ::: MAX_ARMY_EXPERIENCE = 500,
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 999;			--Max navy experience a country can store		#HOI4TGW ::: MAX_NAVY_EXPERIENCE = 500,
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 999;			--Max air experience a country can store		#HOI4TGW ::: MAX_AIR_EXPERIENCE = 500,
